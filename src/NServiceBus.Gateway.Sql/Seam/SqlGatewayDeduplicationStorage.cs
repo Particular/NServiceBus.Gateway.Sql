9c729af731182597ebac857f332b8debfeee8e19
David Boike <david.boike@gmail.com>
2020-01-28 16:37:36 -0600

    Generic implementations of seam interfaces

---
+++ src/NServiceBus.Gateway.Sql/Seam/SqlGatewayDeduplicationStorage.cs
@@ -0,0 +1,16 @@
+﻿using NServiceBus.Extensibility;
+using System;
+using System.Threading.Tasks;
+
+namespace NServiceBus.Gateway.Sql
+{
+    class SqlGatewayDeduplicationStorage : IGatewayDeduplicationStorage
+    {
+        public bool SupportsDistributedTransactions => true;
+
+        public Task<IDeduplicationSession> CheckForDuplicate(string messageId, ContextBag context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
