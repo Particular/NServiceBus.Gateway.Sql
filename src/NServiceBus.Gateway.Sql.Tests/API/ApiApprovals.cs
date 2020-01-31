cddda13e79a44018f8a1f61720df49de4246b3fe
David Boike <david.boike@gmail.com>
2020-01-31 14:22:26 -0600

    API Approvals

---
+++ src/NServiceBus.Gateway.Sql.Tests/API/ApiApprovals.cs
@@ -0,0 +1,19 @@
+﻿namespace NServiceBus.Gateway.Sql.Tests.API
+{
+    using NServiceBus.Gateway.Sql;
+    using NUnit.Framework;
+    using Particular.Approvals;
+    using PublicApiGenerator;
+
+    [TestFixture]
+    class ApiApprovals
+    {
+        [Test]
+        public void ApproveNServiceBusGatewaySqlApi()
+        {
+
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(SqlGatewayDeduplicationConfiguration).Assembly, excludeAttributes: new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" });
+            Approver.Verify(publicApi);
+        }
+    }
+}
