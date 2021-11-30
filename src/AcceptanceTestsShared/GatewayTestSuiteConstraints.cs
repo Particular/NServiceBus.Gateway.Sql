2823f67f247ab54a5c6e99cf5ea391c0e688973b
David Boike <david.boike@gmail.com>
2021-11-30 15:17:14 -0600

    Add support for .NET 6 - release-1.0 (#194)
    * Add support for .NET 6 - release-1.0
    
    * Use new alpha package for test sources fixed for net6.0
    
    * Try updating core packages
    
    * Deal with null endpoint name

--- src/AcceptanceTestsShared/GatewayTestSuiteConstraints.cs
+++ src/AcceptanceTestsShared/GatewayTestSuiteConstraints.cs
@@ -6,20 +6,26 @@ using System.Data.SqlClient;
 using NServiceBus.AcceptanceTesting.Support;
 using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using NServiceBus.Configuration.AdvancedExtensibility;
 
 namespace NServiceBus.Gateway.AcceptanceTests
 {
     public partial class GatewayTestSuiteConstraints
     {
-        public Task<GatewayDeduplicationConfiguration> ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
+        public Task ConfigureDeduplicationStorage(string endpointName, EndpointConfiguration configuration, RunSettings settings)
         {
             var connectionString = DatabaseUtil.GetConnectionString();
+            var cfgSettings = configuration.GetSettings();
+
+            endpointName = endpointName ?? cfgSettings.EndpointName();
 
             var config = new SqlGatewayDeduplicationConfiguration();
             config.TableName = Regex.Replace(endpointName, "[^A-Za-z0-9]+", "") + "_GatewayDeduplication";
             config.ConnectionBuilder(builder => new SqlConnection(connectionString));
 
-            return Task.FromResult<GatewayDeduplicationConfiguration>(config);
+            var gatewaySettings = configuration.Gateway(config);
+            cfgSettings.Set(gatewaySettings);
+            return Task.FromResult(false);
         }
 
         public Task Cleanup()
